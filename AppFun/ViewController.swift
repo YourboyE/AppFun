e1ff753833acd231bcf8232c28f3cabd5690f7ba
YourBoyE <egcrossmover@gmail.com>
2018-01-17 18:08:24 -0800

    Did a full change up
    Added a switch btn
    two text fld boxes
    reconfiged the layout
    app is working properly

--- AppFun/ViewController.swift
+++ AppFun/ViewController.swift
@@ -12,13 +12,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var pressMeBtn: UIButton!
     @IBOutlet weak var customLabel: UILabel!
-    var buttonCount = 0
+    @IBOutlet weak var topTextFld: UITextField!
+    @IBOutlet weak var bottomTextFld: UITextField!
+    @IBOutlet weak var switcher: UISwitch!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        view.backgroundColor = UIColor.cyan
+        view.backgroundColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,19 +30,22 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnTapped(_ sender: Any) {
-        buttonCount = buttonCount + 1
-        
-        if buttonCount <= 10 {
-            customLabel.text = "\(buttonCount)"
-        } else if buttonCount > 10 {
-            customLabel.text = "Your Done"
-            buttonCount = 0
+        if switcher.isOn {
+        let sum = Double(topTextFld.text!)! + Double(bottomTextFld.text!)!
+        customLabel.text = ("\(topTextFld.text!) + \(bottomTextFld.text!) = \(sum)")
+        } else {
+            let sum = Double(topTextFld.text!)! - Double(bottomTextFld.text!)!
+            customLabel.text = ("\(topTextFld.text!) - \(bottomTextFld.text!) = \(sum)")
         }
-        
+    }
+    
+    @IBAction func switchBtn(_ sender: Any) {
+        if switcher.isOn{
+        view.backgroundColor = UIColor.red
+        } else {
+            view.backgroundColor = UIColor.cyan
         }
-    
-    // #getwiththegit
-    
+    }
     
 }
 
